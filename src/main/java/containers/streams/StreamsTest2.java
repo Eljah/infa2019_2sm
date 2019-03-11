5ed3176e35732980d596932eead5114263129803
Eljah <eljah@mail.ru>
2019-03-11 10:43:05 +0300

    Added Optional example

--- src/main/java/containers/streams/StreamsTest2.java
+++ src/main/java/containers/streams/StreamsTest2.java
@@ -1,6 +1,8 @@
 package containers.streams;
 
+import java.security.cert.PKIXRevocationChecker;
 import java.util.*;
+import java.util.function.Supplier;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
@@ -116,14 +118,108 @@ public class StreamsTest2 {
                 }).
                 reduce((x, y) -> x + x)));
 
-        System.out.println((Stream.generate(() -> 1d).limit(100).reduce(0d,(x, y) -> y + x)));
-        System.out.println((Stream.generate(() -> 1d).limit(100).reduce(1d,(x, y) -> y + x)));
-        System.out.println((Stream.generate(() -> 10d).limit(100).reduce(1d,(x, y) -> y * x)));
-        System.out.println((Stream.generate(() -> 10d).limit(100).reduce(0d,(x, y) -> y * x)));
+        System.out.println((Stream.generate(() -> 1d).limit(100).reduce(0d, (x, y) -> y + x)));
+        System.out.println((Stream.generate(() -> 1d).limit(100).reduce(1d, (x, y) -> y + x)));
+        System.out.println((Stream.generate(() -> 10d).limit(100).reduce(1d, (x, y) -> y * x)));
+        System.out.println((Stream.generate(() -> 10d).limit(100).reduce(0d, (x, y) -> y * x)));
 
         System.out.println("1.0".length());
 
-        System.out.println((Stream.generate(() -> 1d).limit(100).map(i -> i.toString()).reduce(0,(total, word)-> total+ word.length(),(total1,total2)->total1+total2)));
+        System.out.println((Stream.generate(() -> 1d).limit(100).map(i -> i.toString()).reduce(0, (total, word) -> total + word.length(), (total1, total2) -> total1 + total2)));
+        // identy, accumulator, combiner
+
+        System.out.println();
+
+        System.out.println((Stream.generate(() -> 1d).limit(10).map(i -> i.toString()).reduce(0, (total, word) -> {
+                    System.out.println("Total " + total + "; word " + word);
+                    return total + word.length();
+                },
+                (total1, total2) -> {
+                    System.out.println("Total1 "+total1+"; total2 "+total2);
+                    return total1 + total2;
+                })));
+
+        System.out.println();
+
+        System.out.println((Stream.generate(() -> 1d).parallel().limit(10).map(i -> i.toString()).reduce(0, (total, word) -> {
+                    System.out.println("Total " + total + "; word " + word);
+                    return total + word.length();
+                },
+                (total1, total2) -> {
+                    System.out.println("Total1 "+total1+"; total2 "+total2);
+                    return total1 + total2;
+                })));
+
+
+        System.out.println();
+
+        Optional<Double> inverseOf1=inverse(1d);
+        Optional<Double> inverseOf0=inverse(0d);
+
+        System.out.println(inverseOf1);
+        System.out.println(inverseOf0);
+
+        Optional<String> string0=string("Something");
+        Optional<String> string1=string("");
+        Optional<String> string2=string(null);
+        Optional<String> string3=string("Another something");
+
+        //Double calculation=1d+inverseOf0.get();
+        Double calculation=inverseOf0.isPresent() ? 1d+inverseOf0.get(): 1d; //hmm why not and better than
+
+        String sum="aa"+string2+string0+string1;
+        //String sum="aa"+string2.get()+string0.get()+string1.get();
+
+        System.out.println("Calculation 1: "+calculation);
+        System.out.println("String sum 1: "+sum);
+
+        Double calculation2=inverseOf0.orElse(-1d);
+        String sum2="aa"+string2.orElse("[none]")+string0.orElse("[none]")+string1.orElse("[none]");
+
+        System.out.println("Calculation 2: "+calculation2);
+        System.out.println("String sum 2: "+sum2);
+
+        Double calculation3=inverseOf0.orElseGet(()->-1d);
+        Supplier supplier=()->{return "empty as on "+new Date().toString();};
+        String sum3="aa"+string2.orElseGet(()->{return "empty as on "+new Date().toString();})+string0.orElseGet(supplier)+string1.orElseGet(supplier);
+
+        System.out.println("Calculation 3: "+calculation3);
+        System.out.println("String sum 3: "+sum3);
+
+        StringBuilder stringBuilder=new StringBuilder();
+
+        Stream<Optional<String>> optionalStream=Arrays.stream(new Optional[]{string0,string1,string2,string3});
+        optionalStream.peek((str)->{
+            System.out.println("Put to StringBuilder "+str);str.ifPresent(stringBuilder::append);}).count();
+        System.out.println("As placed to StringBuilder "+stringBuilder);
+
+        List<Optional<String>> optionalList=new ArrayList<>();
+        optionalList.add(string0);
+        optionalList.add(string1);
+        optionalList.add(string2);
+        optionalList.add(string3);
+
+        StringJoiner stringJoiner=new StringJoiner(", ");
+
+
+        for (Optional<String> stringOptional: optionalList)
+        {
+            stringOptional.ifPresent(stringJoiner::add);
+        }
+
+        System.out.printf("As placed to StringJoiner "+stringJoiner);
+
+    }
+
+    static Optional<Double> inverse(Double value)
+    {
+        return value==0 ? Optional.empty() : Optional.of(1/value);
+    }
+
+    static Optional<String> string(String str)
+    {
+        return Optional.ofNullable(str);
+        //return (str==null || str.isEmpty()) ? Optional.empty() : Optional.ofNullable(str);
 
     }
 }
