65351cd65df3fd896698189a2556f413d82dd001
Ilya <eljah@mail.ru>
2019-03-11 13:28:05 +0300

    Some improvements in class

--- src/main/java/containers/streams/StreamsTest2.java
+++ src/main/java/containers/streams/StreamsTest2.java
@@ -1,6 +1,5 @@
 package containers.streams;
 
-import java.security.cert.PKIXRevocationChecker;
 import java.util.*;
 import java.util.function.Supplier;
 import java.util.stream.Collector;
@@ -14,12 +13,12 @@ import java.util.stream.Stream;
 public class StreamsTest2 {
 
     public static void main(String[] args) {
-        //List<Double> doubleStream=Stream.generate(Math::random).limit(100).sorted().collect(Collectors.toList());
-        List<Double> doubleList = Stream.generate(Math::random).limit(10).sorted().collect(Collectors.toCollection(ArrayList::new));
-
-        System.out.println(doubleList);
-
-        //boolean anyMatchGreaterThan1000=doubleStream.stream().
+        List<Double> doubleList = Stream.generate(Math::random).limit(100).sorted().collect(Collectors.toList());
+//        List<Double> doubleList = Stream.generate(Math::random).limit(10).sorted().collect(Collectors.toCollection(ArrayList::new));
+//
+//        System.out.println(doubleList);
+//
+        //boolean anyMatchGreaterThan1000=doubleList.stream().
         boolean anyMatchGreaterThan1000 = doubleList.parallelStream().
                 peek((doubleValue) -> {
                     System.out.println("Call the first peek: " + doubleValue);
@@ -28,10 +27,11 @@ public class StreamsTest2 {
                     System.out.println("Call the second peek" + doubleValue);
                 }
         )
-                .allMatch((s) -> s > -1000);
-        //.anyMatch((s)->s>-1000);
-        System.out.println(anyMatchGreaterThan1000);
+                //       .allMatch((s) -> s > -1000);
+                .anyMatch((s) -> s > -1000);
 
+        System.out.println(anyMatchGreaterThan1000);
+//
         DoubleStream doubleStream = DoubleStream.generate(Math::random).limit(10000).sorted();
         DoubleSummaryStatistics doubleSummaryStatistics = doubleStream.summaryStatistics();
         System.out.println("Average " + doubleSummaryStatistics.getAverage());
@@ -39,20 +39,22 @@ public class StreamsTest2 {
         System.out.println("Min " + doubleSummaryStatistics.getMin());
         System.out.println("Count " + doubleSummaryStatistics.getCount());
         System.out.println("Sum " + doubleSummaryStatistics.getSum());
-
-        //DoubleSummaryStatistics doubleSummaryStatistics2=Stream.generate(Math::random).limit(10).sorted().collect(Collectors.summarizingDouble(Double::doubleValue));
+//
+        Double d = 1d;
+        d.doubleValue();
+        DoubleSummaryStatistics doubleSummaryStatistics2 = Stream.generate(Math::random).limit(10).sorted().collect(Collectors.summarizingDouble(Double::doubleValue));
         System.out.println("Sum1 " + Stream.generate(Math::random).
-//                parallel().
-//                        peek((doubleValue)->{
-//                            System.out.println("Call the first peek: "+doubleValue);
-//                        }).
-        limit(100).
-//                peek((doubleValue)->{
-//                    System.out.println("Call the second peek: "+doubleValue);
-//                }).
-        sorted().collect(Collectors.summarizingDouble(Double::doubleValue)).getSum());
-
-
+                parallel().
+                peek((doubleValue) -> {
+                    System.out.println("Call the first peek: " + doubleValue);
+                }).
+                limit(100).
+                peek((doubleValue) -> {
+                    System.out.println("Call the second peek: " + doubleValue);
+                }).
+                sorted().collect(Collectors.summarizingDouble(Double::doubleValue)).getSum());
+//
+//
         System.out.println((Stream.generate(Math::random).limit(100).reduce((x, y) -> y + x)));
         System.out.println((Stream.generate(Math::random).limit(100).reduce((x, y) -> y * x)));
         System.out.println((Stream.generate(Math::random).limit(100).reduce((x, y) -> y - x)));
@@ -70,7 +72,8 @@ public class StreamsTest2 {
                 peek((doubleValue) -> {
                     System.out.println("Call the first peek: " + doubleValue);
                 }).
-                reduce((x, y) -> y)));
+                reduce((x, y) -> x + x)));
+
         System.out.println((Stream.generate(() -> 1d).limit(10).
                 peek((doubleValue) -> {
                     System.out.println("Call the first peek: " + doubleValue);
@@ -82,27 +85,27 @@ public class StreamsTest2 {
                     System.out.println("Call the first peek: " + doubleValue);
                 }).
                 reduce((x, y) -> x + x)));
-
-
+//
+//
         System.out.println((Stream.generate(() -> 1d).limit(10).
                 peek((doubleValue) -> {
                     System.out.println("Call the first peek: " + doubleValue);
                 }).
                 reduce((x, y) -> x)));
-
+//
         System.out.println((Stream.generate(() -> 1d).parallel().limit(10).
                 peek((doubleValue) -> {
                     System.out.println("Call the first peek: " + doubleValue);
                 }).map(i -> i.toString()).
                 reduce((x, y) -> x + x)));
-
-
+//
+//
         System.out.println((Stream.generate(() -> 1d).parallel().limit(10).
                 peek((doubleValue) -> {
                     System.out.println("Call the first peek: " + doubleValue);
                 }).
                 reduce((x, y) -> y + y)));
-
+//
         System.out.println((Stream.generate(() -> 1d).parallel().limit(10).
                 peek((doubleValue) -> {
                     System.out.println("Call the first peek: " + doubleValue);
@@ -111,18 +114,20 @@ public class StreamsTest2 {
                     System.out.println("x=" + x + ", y=" + y);
                     return x + x;
                 })));
-
+//
         System.out.println((Stream.<Double>empty().
                 peek((doubleValue) -> {
                     System.out.println("Call the first peek: " + doubleValue);
                 }).
                 reduce((x, y) -> x + x)));
-
-        System.out.println((Stream.generate(() -> 1d).limit(100).reduce(0d, (x, y) -> y + x)));
-        System.out.println((Stream.generate(() -> 1d).limit(100).reduce(1d, (x, y) -> y + x)));
-        System.out.println((Stream.generate(() -> 10d).limit(100).reduce(1d, (x, y) -> y * x)));
-        System.out.println((Stream.generate(() -> 10d).limit(100).reduce(0d, (x, y) -> y * x)));
-
+//
+        System.out.println((Stream.generate(() -> 1d).limit(100).reduce(0d, (x, y) -> x + y)));
+        System.out.println((Stream.generate(() -> 1d).limit(100).reduce(1d, (x, y) -> x + y)));
+        System.out.println((Stream.generate(() -> 10d).limit(100).reduce(1d, (x, y) -> x * y)));
+        System.out.println((Stream.generate(() -> 10d).limit(100).reduce(0d, (x, y) -> x * y)));
+        System.out.println((Stream.generate(() -> 10d).limit(100).reduce(0d, (x, y) -> y - x)));
+        System.out.println((Stream.generate(() -> 10d).limit(100).reduce(0d, (x, y) -> x - y)));
+//
         System.out.println("1.0".length());
 
         System.out.println((Stream.generate(() -> 1d).limit(100).map(i -> i.toString()).reduce(0, (total, word) -> total + word.length(), (total1, total2) -> total1 + total2)));
@@ -135,91 +140,97 @@ public class StreamsTest2 {
                     return total + word.length();
                 },
                 (total1, total2) -> {
-                    System.out.println("Total1 "+total1+"; total2 "+total2);
+                    System.out.println("Total1 " + total1 + "; total2 " + total2);
                     return total1 + total2;
                 })));
 
         System.out.println();
-
-        System.out.println((Stream.generate(() -> 1d).parallel().limit(10).map(i -> i.toString()).reduce(0, (total, word) -> {
+//
+        System.out.println((Stream.generate(() -> 1d).parallel().limit(100).map(i -> i.toString()).reduce(0, (total, word) -> {
                     System.out.println("Total " + total + "; word " + word);
                     return total + word.length();
                 },
                 (total1, total2) -> {
-                    System.out.println("Total1 "+total1+"; total2 "+total2);
+                    System.out.println("Total1 " + total1 + "; total2 " + total2);
                     return total1 + total2;
                 })));
 
 
         System.out.println();
-
-        Optional<Double> inverseOf1=inverse(1d);
-        Optional<Double> inverseOf0=inverse(0d);
+//
+        Optional<Double> inverseOf1 = inverse(1d);
+        Optional<Double> inverseOf0 = inverse(0d);
 
         System.out.println(inverseOf1);
         System.out.println(inverseOf0);
-
-        Optional<String> string0=string("Something");
-        Optional<String> string1=string("");
-        Optional<String> string2=string(null);
-        Optional<String> string3=string("Another something");
-
+//
+        Optional<String> string0 = string("Something");
+        Optional<String> string1 = string("");
+        Optional<String> string2 = string(null);
+        Optional<String> string3 = string("Another something");
+//
+        Double calculation0 = 1d + inverseOf1.get();
+        System.out.println(calculation0);
         //Double calculation=1d+inverseOf0.get();
-        Double calculation=inverseOf0.isPresent() ? 1d+inverseOf0.get(): 1d; //hmm why not and better than
+        Double calculation = inverseOf0.isPresent() ? 1d + inverseOf0.get() : 1d; //hmm why not and better than
 
-        String sum="aa"+string2+string0+string1;
+        System.out.println("Calculation 1: " + calculation);
+
+        String sum = "aa" + string2 + string0 + string1;
         //String sum="aa"+string2.get()+string0.get()+string1.get();
+//
+        System.out.println("String sum 1: " + sum);
+//
+        Double calculation2 = inverseOf0.orElse(-1d);
+        String sum2 = "aa" + string2.orElse("[none]") + string0.orElse("[none]") + string1.orElse("[none]");
 
-        System.out.println("Calculation 1: "+calculation);
-        System.out.println("String sum 1: "+sum);
+        System.out.println("Calculation 2: " + calculation2);
+        System.out.println("String sum 2: " + sum2);
+//
+        Double calculation3 = inverseOf0.orElseGet(() -> -1d);
+        Supplier supplier = () -> {
+            return "empty as on " + new Date().toString();
+        };
+        String sum3 = "aa" + string2.orElseGet(() -> {
+            return "empty as on " + new Date().toString();
+        }) + string0.orElseGet(supplier) + string1.orElseGet(supplier);
 
-        Double calculation2=inverseOf0.orElse(-1d);
-        String sum2="aa"+string2.orElse("[none]")+string0.orElse("[none]")+string1.orElse("[none]");
+        System.out.println("Calculation 3: " + calculation3);
+        System.out.println("String sum 3: " + sum3);
+//
+        StringBuilder stringBuilder = new StringBuilder();
 
-        System.out.println("Calculation 2: "+calculation2);
-        System.out.println("String sum 2: "+sum2);
+        Stream<Optional<String>> optionalStream = Arrays.stream(new Optional[]{string0, string1, string2, string3});
+        optionalStream.peek((str) -> {
+            System.out.println("Put to StringBuilder " + str);
+            str.ifPresent(stringBuilder::append);
+        }).count();
+        System.out.println("As placed to StringBuilder " + stringBuilder);
 
-        Double calculation3=inverseOf0.orElseGet(()->-1d);
-        Supplier supplier=()->{return "empty as on "+new Date().toString();};
-        String sum3="aa"+string2.orElseGet(()->{return "empty as on "+new Date().toString();})+string0.orElseGet(supplier)+string1.orElseGet(supplier);
-
-        System.out.println("Calculation 3: "+calculation3);
-        System.out.println("String sum 3: "+sum3);
-
-        StringBuilder stringBuilder=new StringBuilder();
-
-        Stream<Optional<String>> optionalStream=Arrays.stream(new Optional[]{string0,string1,string2,string3});
-        optionalStream.peek((str)->{
-            System.out.println("Put to StringBuilder "+str);str.ifPresent(stringBuilder::append);}).count();
-        System.out.println("As placed to StringBuilder "+stringBuilder);
-
-        List<Optional<String>> optionalList=new ArrayList<>();
+        List<Optional<String>> optionalList = new ArrayList<>();
         optionalList.add(string0);
         optionalList.add(string1);
         optionalList.add(string2);
         optionalList.add(string3);
 
-        StringJoiner stringJoiner=new StringJoiner(", ");
+        StringJoiner stringJoiner = new StringJoiner(", ");
 
-
-        for (Optional<String> stringOptional: optionalList)
-        {
+        for (Optional<String> stringOptional : optionalList) {
             stringOptional.ifPresent(stringJoiner::add);
         }
 
-        System.out.printf("As placed to StringJoiner "+stringJoiner);
+        System.out.printf("As placed to StringJoiner " + stringJoiner);
 
     }
 
-    static Optional<Double> inverse(Double value)
-    {
-        return value==0 ? Optional.empty() : Optional.of(1/value);
+    //
+    static Optional<Double> inverse(Double value) {
+        return value == 0 ? Optional.empty() : Optional.of(1 / value);
     }
 
-    static Optional<String> string(String str)
-    {
-        return Optional.ofNullable(str);
-        //return (str==null || str.isEmpty()) ? Optional.empty() : Optional.ofNullable(str);
+    static Optional<String> string(String str) {
+        //return Optional.ofNullable(str);
+        return (str==null || str.isEmpty()) ? Optional.empty() : Optional.ofNullable(str);
 
     }
 }
