31c7d8794e9015f0f9527938451eeb2f7c26dcb3
Eljah <eljah@mail.ru>
2019-03-11 08:42:09 +0300

    parallel to generate math random example

--- src/main/java/containers/streams/StreamsTest2.java
+++ src/main/java/containers/streams/StreamsTest2.java
@@ -1,11 +1,9 @@
 package containers.streams;
 
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
 import java.util.stream.Stream;
 
 /**
@@ -15,12 +13,12 @@ public class StreamsTest2 {
 
     public static void main(String[] args) {
         //List<Double> doubleStream=Stream.generate(Math::random).limit(100).sorted().collect(Collectors.toList());
-        List<Double> doubleStream=Stream.generate(Math::random).limit(10).sorted().collect(Collectors.toCollection(ArrayList::new));
+        List<Double> doubleList=Stream.generate(Math::random).limit(10).sorted().collect(Collectors.toCollection(ArrayList::new));
 
-        System.out.println(doubleStream);
+        System.out.println(doubleList);
 
         //boolean anyMatchGreaterThan1000=doubleStream.stream().
-        boolean anyMatchGreaterThan1000=doubleStream.parallelStream().
+        boolean anyMatchGreaterThan1000=doubleList.parallelStream().
                 peek((doubleValue)->{
             System.out.println("Call the first peek: "+doubleValue);
         }).peek(
@@ -31,6 +29,28 @@ public class StreamsTest2 {
         .allMatch((s)->s>-1000);
         //.anyMatch((s)->s>-1000);
         System.out.println(anyMatchGreaterThan1000);
+
+        DoubleStream doubleStream=DoubleStream.generate(Math::random).limit(10000).sorted();
+        DoubleSummaryStatistics doubleSummaryStatistics=doubleStream.summaryStatistics();
+        System.out.println("Average "+doubleSummaryStatistics.getAverage());
+        System.out.println("Max "+doubleSummaryStatistics.getMax());
+        System.out.println("Min "+doubleSummaryStatistics.getMin());
+        System.out.println("Count "+doubleSummaryStatistics.getCount());
+        System.out.println("Sum "+doubleSummaryStatistics.getSum());
+
+        //DoubleSummaryStatistics doubleSummaryStatistics2=Stream.generate(Math::random).limit(10).sorted().collect(Collectors.summarizingDouble(Double::doubleValue));
+        System.out.println("Sum1 "+Stream.generate(Math::random).
+//                parallel().
+//                        peek((doubleValue)->{
+//                            System.out.println("Call the first peek: "+doubleValue);
+//                        }).
+                limit(100).
+//                peek((doubleValue)->{
+//                    System.out.println("Call the second peek: "+doubleValue);
+//                }).
+                        sorted().collect(Collectors.summarizingDouble(Double::doubleValue)).getSum());
+
+
 
 
     }
