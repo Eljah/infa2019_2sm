6bcc464e646180d7fbf1788115c6337c9cebbfdd
Eljah <eljah@mail.ru>
2019-03-12 17:07:30 +0300

    mapping with Collector.toMap

--- src/main/java/containers/streams/StreamsTest3.java
+++ src/main/java/containers/streams/StreamsTest3.java
@@ -2,10 +2,7 @@ package containers.streams;
 
 import containers.common.Student;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
@@ -17,27 +14,27 @@ import java.util.stream.Stream;
 public class StreamsTest3 {
     public static void main(String[] args) {
 
-        Optional<StudentInGroup> petrov=Optional.of(new StudentInGroup("Petrov",null,18,false));
-        Optional<StudentInGroup> petrova=Optional.of(new StudentInGroup("Petrova","11-305",18,true));
+        Optional<StudentInGroup> petrov = Optional.of(new StudentInGroup("Petrov", null, 18, false));
+        Optional<StudentInGroup> petrova = Optional.of(new StudentInGroup("Petrova", "11-305", 18, true));
 
-        List<Optional<StudentInGroup>> studentInGroupList=new ArrayList<>();
-        studentInGroupList.add(Optional.of(new StudentInGroup("Ivanov","11-307",18,false)));
-        studentInGroupList.add(Optional.of(new StudentInGroup("Johns","11-307",19,false)));
-        studentInGroupList.add(Optional.of(new StudentInGroup("Walitov","11-307",17,false)));
-        studentInGroupList.add(Optional.of(new StudentInGroup("Kowalski","11-302",19,false)));
-        studentInGroupList.add(Optional.of(new StudentInGroup("Glauberová","11-302",19,true)));
-        studentInGroupList.add(Optional.of(new StudentInGroup("Dybenko","11-307",17,true)));
-        studentInGroupList.add(Optional.of(new StudentInGroup("Ishikawa","11-305",18,false)));
-        studentInGroupList.add(Optional.of(new StudentInGroup("Nguyen","11-305",18,false)));
-        studentInGroupList.add(Optional.of(new StudentInGroup("Chui","11-305",18,true)));
+        List<Optional<StudentInGroup>> studentInGroupList = new ArrayList<>();
+        studentInGroupList.add(Optional.of(new StudentInGroup("Ivanov", "11-307", 18, false)));
+        studentInGroupList.add(Optional.of(new StudentInGroup("Johns", "11-307", 19, false)));
+        studentInGroupList.add(Optional.of(new StudentInGroup("Walitov", "11-307", 17, false)));
+        studentInGroupList.add(Optional.of(new StudentInGroup("Kowalski", "11-302", 19, false)));
+        studentInGroupList.add(Optional.of(new StudentInGroup("Glauberová", "11-302", 19, true)));
+        studentInGroupList.add(Optional.of(new StudentInGroup("Dybenko", "11-307", 17, true)));
+        studentInGroupList.add(Optional.of(new StudentInGroup("Ishikawa", "11-305", 18, false)));
+        studentInGroupList.add(Optional.of(new StudentInGroup("Nguyen", "11-305", 18, false)));
+        studentInGroupList.add(Optional.of(new StudentInGroup("Chui", "11-305", 18, true)));
         studentInGroupList.add(petrov);
         studentInGroupList.add(Optional.empty());
         studentInGroupList.add(petrova);
 
-        String petrovasGroup=petrova.get().getGroupNumber().get(); //is it ok and null safe?
-        Optional<String> petrovasGroup1=studentInGroupList.get(0).flatMap(StudentInGroup::getGroupNumber);
-        Optional<String> petrovasGroup2=petrova.flatMap(StudentInGroup::getGroupNumber);
-        Optional<String> petrovsGroup=petrov.flatMap(StudentInGroup::getGroupNumber);
+        String petrovasGroup = petrova.get().getGroupNumber().get(); //is it ok and null safe?
+        Optional<String> petrovasGroup1 = studentInGroupList.get(0).flatMap(StudentInGroup::getGroupNumber);
+        Optional<String> petrovasGroup2 = petrova.flatMap(StudentInGroup::getGroupNumber);
+        Optional<String> petrovsGroup = petrov.flatMap(StudentInGroup::getGroupNumber);
 
         System.out.println(petrovasGroup);
         System.out.println(petrovasGroup1);
@@ -46,24 +43,84 @@ public class StreamsTest3 {
 
         System.out.println();
 
-        for (Optional<StudentInGroup> studentOptional: studentInGroupList)
-        {
+        for (Optional<StudentInGroup> studentOptional : studentInGroupList) {
             //System.out.println(studentOptional.orElse("There is no student data")); not good idea for the object
             //System.out.println(studentOptional.orElseGet(()->"There is no student data")); not good idea for the object
 
-            System.out.println("Common list item: "+studentOptional);
+            System.out.println("Common list item: " + studentOptional);
             studentOptional.ifPresent(System.out::println);
-            studentOptional.ifPresent(s->System.out.println(s.getGroupNumber()));
-            studentOptional.ifPresent(s->s.getGroupNumber().ifPresent(System.out::println));
-            studentOptional.ifPresent(s->s.getGroupNumber().ifPresent(group->System.out.println("Group name for the student version 1: "+group)));
-            studentOptional.flatMap(StudentInGroup::getGroupNumber).ifPresent(group->System.out.println("Group name for the student version 2: "+group));
+            studentOptional.ifPresent(s -> System.out.println(s.getGroupNumber()));
+            studentOptional.ifPresent(s -> s.getGroupNumber().ifPresent(System.out::println));
+            studentOptional.ifPresent(s -> s.getGroupNumber().ifPresent(group -> System.out.println("Group name for the student version 1: " + group)));
+            studentOptional.flatMap(StudentInGroup::getGroupNumber).ifPresent(group -> System.out.println("Group name for the student version 2: " + group));
         }
 
 
-        Supplier<Stream<StudentInGroup>> studentInGroupStreamSupplier = ()-> studentInGroupList.stream().map((o)->o.orElse(new StudentInGroup(null,null,-1,false)));
+        Supplier<Stream<StudentInGroup>> studentInGroupStreamSupplier = () -> studentInGroupList.stream().map((o) -> o.orElse(new StudentInGroup(null, null, -1, false)));
 
-        Map<String,StudentInGroup> stringStudentInGroupMap=studentInGroupStreamSupplier.get().collect(Collectors.toMap(StudentInGroup::getName, Function.identity()));
+        Map<String, StudentInGroup> stringStudentInGroupMap = studentInGroupStreamSupplier.get().collect(Collectors.toMap(StudentInGroup::getName, (l) -> l));
         System.out.println(stringStudentInGroupMap);
+
+//        Map<Integer,StudentInGroup> stringStudentInGroupMap1=studentInGroupStreamSupplier.get().collect(Collectors.toMap(StudentInGroup::getAge, Function.identity()));
+//        System.out.println(stringStudentInGroupMap1);
+
+        Map<Integer, StudentInGroup> stringStudentInGroupMap1 = studentInGroupStreamSupplier.get().collect(Collectors.toMap(StudentInGroup::getAge, Function.identity(), (oldI, newI) -> newI));
+        System.out.println(stringStudentInGroupMap1);
+
+        Map<Integer, StudentInGroup> stringStudentInGroupMap2 = studentInGroupStreamSupplier.get().collect(Collectors.toMap(StudentInGroup::getAge, Function.identity(), (oldI, newI) -> oldI));
+        System.out.println(stringStudentInGroupMap2);
+
+        Map<Integer, Set<StudentInGroup>> stringStudentInGroupMap3 = studentInGroupStreamSupplier.<StudentInGroup>get().collect(Collectors.toMap(StudentInGroup::getAge, Collections::<StudentInGroup>singleton,
+                (Set<StudentInGroup> a, Set<StudentInGroup> b) -> {
+                    Set<StudentInGroup> studentInGroupSet = new HashSet<>();
+                    studentInGroupSet.addAll(a);
+                    studentInGroupSet.addAll(b);
+                    return studentInGroupSet;
+                }));
+        System.out.println(stringStudentInGroupMap3);
+
+
+        Map<Integer, Set<StudentInGroup>> stringStudentInGroupMap4 = studentInGroupStreamSupplier.<StudentInGroup>get().collect(Collectors.toMap(StudentInGroup::getAge, x -> new HashSet<>(Arrays.asList(x)),
+                (x, y) -> {
+                    x.addAll(y);
+                    return x;
+                }));
+        System.out.println(stringStudentInGroupMap4);
+
+//
+//        Map<Integer, Set<StudentInGroup>> stringStudentInGroupMap6 = studentInGroupStreamSupplier.<StudentInGroup>get().collect(Collectors.toMap(StudentInGroup::getAge, Collections::<StudentInGroup>singleton,
+//                (x, y) -> {
+//                    x.addAll(y);
+//                    return x;
+//                }));
+//        System.out.println(stringStudentInGroupMap6);
+
+        Map<Integer, Set<StudentInGroup>> stringStudentInGroupMap5 = studentInGroupStreamSupplier.<StudentInGroup>get().collect(Collectors.toMap(StudentInGroup::getAge, x -> {System.out.println("x: "+x); return new HashSet<>();},
+                (x, y) -> {
+                    System.out.println("x: "+x+", y: "+y);
+                    x.addAll(y);
+                    return x;
+                }
+                ,HashMap::new));
+        System.out.println(stringStudentInGroupMap5);
+
+        Map<Integer, Set<StudentInGroup>> stringStudentInGroupMap7 = studentInGroupStreamSupplier.<StudentInGroup>get().collect(Collectors.toMap(StudentInGroup::getAge, x -> {System.out.println("x: "+x); return new HashSet<>(Arrays.asList(x));},
+                (x, y) -> {
+                    System.out.println("x: "+x+", y: "+y);
+                    x.addAll(y);
+                    return x;
+                }
+                ,HashMap::new));
+        System.out.println(stringStudentInGroupMap7);
+
+        Map<Integer, Set<StudentInGroup>> stringStudentInGroupMap8 = studentInGroupStreamSupplier.<StudentInGroup>get().parallel().collect(Collectors.toMap(StudentInGroup::getAge, x -> {System.out.println("x: "+x); return new HashSet<>();},
+                (x, y) -> {
+                    System.out.println("x: "+x+", y: "+y);
+                    x.addAll(y);
+                    return x;
+                }
+                ,HashMap::new));
+        System.out.println(stringStudentInGroupMap8);
 
 
     }
