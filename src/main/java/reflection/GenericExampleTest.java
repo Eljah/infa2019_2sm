7b0e5cc9ecf6bf17c7339c40991047f08e17f1b6
Ilya <eljah@mail.ru>
2019-03-25 07:24:56 +0300

    reflection proxy added

--- src/main/java/reflection/GenericExampleTest.java
+++ src/main/java/reflection/GenericExampleTest.java
@@ -6,43 +6,45 @@ import containers.set.OwnArraySet;
 import containers.set.OwnLinkedSet;
 import containers.streams.StudentInGroup;
 
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Collections;
 
 public class GenericExampleTest {
 
     public static void main(String[] args) {
-        String field="aaa";
+        String field = "aaa";
 
         //загрузка
         //компоновка
         //инициализация
 
-        Displayeble displayeble=new OwnLinkedSet();
+        Displayeble displayeble = new OwnLinkedSet();
         OwnLinkedSet.justBe();
         //OwnArraySet.justBe(); //initialized on static method call
         //OwnArraySet.b=200; //initialized on static variable call
         //System.out.println(OwnArraySet.C); //not initialzed on final stativ variable call
         System.out.println(OwnArraySet.b); //initialized on static variable call
-        Class displayebleClass=OwnArraySet.class;
+        Class displayebleClass = OwnArraySet.class;
         System.out.println(displayebleClass.isInstance(displayeble));
 
-        Class stringClass;;//= (Class<String>) field.<String>getClass();
+        Class stringClass;
+        ;//= (Class<String>) field.<String>getClass();
 
         try {
-            stringClass=Class.forName("reflection.StudentDescender");
+            stringClass = Class.forName("reflection.StudentDescender");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
 
-        GenericExample<String> stringGenericExample=new GenericExample<>("aaa");
-        GenericExample<StudentInGroup> studentInGroupGenericExample=new GenericExample<>(new StudentInGroup("GroupStudent","11-806",18,false));
-        GenericExample<StudentDescender> studentInGroupGenericExample1=new GenericExample<>(new StudentDescender("GroupStudent","11-806",18,false));
-        GenericExample<Displayeble> studentInGroupGenericExample2=new GenericExample<>(new OwnArraySet(100));
-        GenericExample<Student> studentGenericExample=new GenericExample<>(new Student("aa","bb"));
+        GenericExample<String> stringGenericExample = new GenericExample<>("aaa");
+        GenericExample<StudentInGroup> studentInGroupGenericExample = new GenericExample<>(new StudentInGroup("GroupStudent", "11-806", 18, false));
+        GenericExample<StudentDescender> studentInGroupGenericExample1 = new GenericExample<>(new StudentDescender("GroupStudent", "11-806", 18, false));
+        GenericExample<Displayeble> studentInGroupGenericExample2 = new GenericExample<>(new OwnArraySet(100));
+        GenericExample<Student> studentGenericExample = new GenericExample<>(new Student("aa", "bb"));
 
 
-        Class classOfParameter=Displayeble.class;
+        Class classOfParameter = Displayeble.class;
         System.out.println(classOfParameter.getSimpleName());
         System.out.println(classOfParameter.getCanonicalName());
         System.out.println(classOfParameter.toString());
@@ -51,6 +53,28 @@ public class GenericExampleTest {
         //System.out.println(classOfParameter.getSuperclass().getName());
         System.out.println(Collections.unmodifiableList(Arrays.asList(classOfParameter.getInterfaces())));
 
+        //Class newClass=new Class();
+        Displayeble displayebleViaProxyCreated = (Displayeble) java.lang.reflect.Proxy.newProxyInstance(Displayeble.class.getClassLoader(), new Class[]{Displayeble.class}, new java.lang.reflect.InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                String method_name = method.getName();
+                Class<?>[] classes = method.getParameterTypes();
 
+                if (method_name.equals("display")) {
+                    if (args == null) {
+                        System.out.println("Display called without args");
+                        return null;
+                    } else {
+                        System.out.println("Display called with args but how it is possible because of methiod signature?");
+                    }
+                }
+                return null;
+            }
+        });
+        displayebleViaProxyCreated.display();
+
+        System.out.println(Displayeble.class.getClassLoader().toString());
+        System.out.println(Displayeble.class.getClassLoader().getParent());
     }
+
 }
