61f592dfecc70ca976f2c8302196c62102f8ea39
Ilya <eljah@mail.ru>
2019-03-25 08:13:09 +0300

    strange behavior of methods result substitution

--- src/main/java/reflection/SimpleProxyDemo.java
+++ src/main/java/reflection/SimpleProxyDemo.java
@@ -32,9 +32,18 @@ public class SimpleProxyDemo
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
             System.out.println("THIS IS BEFORE CALL FROM DYNAMIC PROXY, CALLING METHOD "+method.getName());
+            if (args!=null&&args.length>0) {
+                args[0] = args[0] + "I DO INFLUENCE";
+            }
+            //Object toBeReturned= method.invoke(proxied,args+"I DO INFLUENCE");
             Object toBeReturned= method.invoke(proxied,args);
             System.out.println("THIS IS AFTER CALL FROM DYNAMIC PROXY, CALLING METHOD "+method.getName());
-            return toBeReturned;
+            //if (toBeReturned instanceof String) {
+            if (toBeReturned !=null) {
+                return "OLOLO I CAN INFLUENCE";
+            }
+            else
+                return toBeReturned;
         }
     }
 }
