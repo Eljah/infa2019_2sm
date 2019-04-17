a5c6ede2c80584263fb65b3bcfa1e48e20aabf26
Eljah <eljah@mail.ru>
2019-04-17 07:15:59 +0300

    adding buffer unmap and read

--- src/main/java/files/FileTest3.java
+++ src/main/java/files/FileTest3.java
@@ -1,5 +1,8 @@
 package files;
 
+import sun.nio.ch.DirectBuffer;
+
+import java.io.File;
 import java.io.IOException;
 import java.nio.CharBuffer;
 import java.nio.MappedByteBuffer;
@@ -16,17 +19,22 @@ import java.util.EnumSet;
  */
 public class FileTest3 {
     public static void main(String[] args) throws IOException {
-        String filename="example4.txt";
-        Path pathToFile= Paths.get(filename);
+        String filename = "example4.txt";
+        Path pathToFile = Paths.get(filename);
 
         //CharBuffer charBuffer = null;
 
         CharBuffer charBuffer = CharBuffer
                 .wrap("This will be written to the file");
 
-        if (!Files.exists(pathToFile)) {
-            Files.createFile(pathToFile);
-        }
+//        if (!Files.exists(pathToFile)) {
+//            Files.createFile(pathToFile);
+//        }
+
+        File file = new File(filename);
+        if (file.createNewFile()) {
+            System.out.println(filename + " File Created");
+        } else System.out.println("File " + filename + " already exists");
 
         try (FileChannel fileChannel = (FileChannel) Files
                 .newByteChannel(pathToFile, EnumSet.of(
@@ -41,6 +49,14 @@ public class FileTest3 {
                 mappedByteBuffer.put(
                         Charset.forName("utf-8").encode(charBuffer));
             }
+            //fileChannel.close();
+            unmap(mappedByteBuffer);
+        }
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
         try (FileChannel fileChannel = (FileChannel) Files.newByteChannel(
@@ -54,8 +70,48 @@ public class FileTest3 {
             }
 
             System.out.println(charBuffer.toString());
+            //fileChannel.close();
+            unmap(mappedByteBuffer);
         }
 
+        long fileSize = 0;
+        try (FileChannel fileChannel = (FileChannel) Files.newByteChannel(
+                pathToFile, EnumSet.of(StandardOpenOption.READ))) {
 
+            fileSize = fileChannel.size();
+            System.out.format("File size is %d", fileSize);
+            //fileChannel.close();
+        }
+
+        try (FileChannel fileChannel = (FileChannel) Files.newByteChannel(
+                pathToFile, EnumSet.of(StandardOpenOption.READ))) {
+
+            try {
+                Thread.sleep(30000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            long fileSizeNew = fileChannel.size();
+            System.out.format("New file size is %d", fileSizeNew);
+            MappedByteBuffer mappedByteBuffer = fileChannel
+                    .map(FileChannel.MapMode.READ_ONLY, fileSize, fileSizeNew - fileSize);
+
+            if (mappedByteBuffer != null) {
+                charBuffer = Charset.forName("UTF-8").decode(mappedByteBuffer);
+                System.out.println(charBuffer.position());
+                System.out.println(charBuffer.hasRemaining());
+                //if (charBuffer.hasRemaining()) {
+                System.out.println();
+                System.out.format("Another process wrote: %s", charBuffer.toString());
+                //}
+            }
+            fileSize = fileChannel.size();
+            unmap(mappedByteBuffer);
+        }
+    }
+
+    public static void unmap(MappedByteBuffer buffer) {
+        sun.misc.Cleaner cleaner = ((DirectBuffer) buffer).cleaner();
+        cleaner.clean();
     }
 }
