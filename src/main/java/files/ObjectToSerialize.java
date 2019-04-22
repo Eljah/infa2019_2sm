402d93e3c80edd31e2d6b8cbb253adb71119b61e
Eljah <eljah@mail.ru>
2019-04-22 09:19:19 +0300

    externalization example added

--- src/main/java/files/ObjectToSerialize.java
+++ src/main/java/files/ObjectToSerialize.java
@@ -1,11 +1,13 @@
 package files;
 
-import java.io.Serializable;
+import java.io.*;
 
 /**
  * Created by eljah32 on 4/22/2019.
  */
-public class ObjectToSerialize implements Serializable {
+public class ObjectToSerialize implements
+        //Serializable,
+        Externalizable {
 
     private static String staticExample="static";
 
@@ -16,9 +18,12 @@ public class ObjectToSerialize implements Serializable {
     //transient
     private String isResurrected;
 
+    public ObjectToSerialize()
+    {};
+
     ObjectToSerialize(String name, int age, boolean gender)
     {
-        System.out.println("Write here is the constructior was used");
+        System.out.println("Write here if the constructior was used");
         this.name=name;
         this.age=age;
         this.gender=gender;
@@ -31,4 +36,16 @@ public class ObjectToSerialize implements Serializable {
         return "Name:" + name + "\nAge: " + age + "\nGender: " + gender+ "\nIs Resurrected: " + isResurrected;
     }
 
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        System.out.println("Writing");
+        out.writeObject(this.name);
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        System.out.println("Reading");
+        this.name=(String)in.readObject();
+        System.out.println("Read: "+name);
+    }
 }
