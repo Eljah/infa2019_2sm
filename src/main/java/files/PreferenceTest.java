dde9ba26d59fbd02cf79d0f9f1732d45479cb92d
Eljah <eljah@mail.ru>
2019-04-23 22:59:36 +0300

    preferences added

---
+++ src/main/java/files/PreferenceTest.java
@@ -0,0 +1,40 @@
+package files;
+
+import java.util.prefs.Preferences;
+
+/**
+ * Created by eljah32 on 4/23/2019.
+ */
+public class PreferenceTest {
+    private Preferences prefs;
+
+    public void setPreference() {
+        // This will define a node in which the preferences can be stored
+        prefs = Preferences.userRoot().node(this.getClass().getName()+"b");
+        String ID1 = "Test1";
+        String ID2 = "Test2";
+        String ID3 = "Test3";
+
+        // First we will get the values
+        // Define a boolean value
+        System.out.println(prefs.getBoolean(ID1, true));
+        // Define a string with default "Hello World
+        System.out.println(prefs.get(ID2, "Hello World"));
+        // Define a integer with default 50
+        System.out.println(prefs.getInt(ID3, 50));
+
+        // now set the values
+        prefs.putBoolean(ID1, false);
+        prefs.put(ID2, "Hello Europa");
+        prefs.putInt(ID3, 45);
+
+        // Delete the preference settings for the first value
+        prefs.remove(ID1);
+
+    }
+
+    public static void main(String[] args) {
+        PreferenceTest test = new PreferenceTest();
+        test.setPreference();
+    }
+}
